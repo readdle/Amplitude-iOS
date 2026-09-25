2f6b6ca00ebdfea9616501fe19871c028cb8617c
Andrii Selivanov <aselivanov@readdle.com>
2026-09-25 14:35:41 +0300

    Replace Amplitude mac address retrieval method which fails on macOS 27 with another approach used in ReaddleLib. Copy code from ReaddleLib, because Amplitude can not depend on it directly.

--- Sources/Amplitude/AMPDeviceInfo.m
+++ Sources/Amplitude/AMPDeviceInfo.m
@@ -24,6 +24,7 @@
 #import "AMPConstants.h"
 #import "AMPDeviceInfo.h"
 #import "AMPUtils.h"
+#import "RDSystemInformation.h"
 
 #import <sys/sysctl.h>
 #import <sys/types.h>
@@ -32,8 +33,6 @@
 #import <UIKit/UIKit.h>
 #else
 #import <Cocoa/Cocoa.h>
-#import <net/if.h>
-#import <net/if_dl.h>
 #endif
 
 #ifndef AMPLITUDE_LOG
@@ -348,71 +347,16 @@
 
 // For mac only!!!
 #if TARGET_OS_OSX
+// Reworked comparing to original implementation due to https://readdle-j.atlassian.net/browse/PEM-10525
+// Since macOS 27 the original method started reporting 02:00:00:00:00:00 always.
+// Using IOKit instead (porting the RDGetMACAddressString from ReaddleLib), which works fine.
 + (NSString *)getMacAddress {
-    int                 mgmtInfoBase[6];
-    char                *msgBuffer = NULL;
-    size_t              length;
-    unsigned char       macAddress[6];
-    struct if_msghdr    *interfaceMsgStruct;
-    struct sockaddr_dl  *socketStruct;
-    NSString            *errorFlag = NULL;
-    bool                msgBufferAllocated = false;
-
-    // Setup the management Information Base (mib)
-    mgmtInfoBase[0] = CTL_NET;        // Request network subsystem
-    mgmtInfoBase[1] = AF_ROUTE;       // Routing table info
-    mgmtInfoBase[2] = 0;
-    mgmtInfoBase[3] = AF_LINK;        // Request link layer information
-    mgmtInfoBase[4] = NET_RT_IFLIST;  // Request all configured interfaces
-
-    // With all configured interfaces requested, get handle index
-    if ((mgmtInfoBase[5] = if_nametoindex("en0")) == 0) {
-        errorFlag = @"if_nametoindex failure";
-    } else {
-        // Get the size of the data available (store in len)
-        if (sysctl(mgmtInfoBase, 6, NULL, &length, NULL, 0) < 0) {
-            errorFlag = @"sysctl mgmtInfoBase failure";
-        } else {
-            // Alloc memory based on above call
-            if ((msgBuffer = malloc(length)) == NULL) {
-                errorFlag = @"buffer allocation failure";
-            } else {
-                msgBufferAllocated = true;
-                // Get system information, store in buffer
-                if (sysctl(mgmtInfoBase, 6, msgBuffer, &length, NULL, 0) < 0) {
-                    errorFlag = @"sysctl msgBuffer failure";
-                }
-            }
-        }
-    }
-
-    // Before going any further...
-    if (errorFlag != NULL) {
-        AMPLITUDE_LOG(@"Cannot detect mac address. Error: %@", errorFlag);
-        if (msgBufferAllocated) {
-            free(msgBuffer);
-        }
+    NSString *macAddress = AMPGetMACAddressString();
+    if (macAddress.length == 0) {
         return nil;
     }
-
-    // Map msgbuffer to interface message structure
-    interfaceMsgStruct = (struct if_msghdr *)msgBuffer;
-
-    // Map to link-level socket structure
-    socketStruct = (struct sockaddr_dl *)(interfaceMsgStruct + 1);
-
-    // Copy link layer address data in socket structure to an array
-    memcpy(&macAddress, socketStruct->sdl_data + socketStruct->sdl_nlen, 6);
-
-    // Read from char array into a string object, into traditional Mac address format
-    NSString *macAddressString = [NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X",
-                                  macAddress[0], macAddress[1], macAddress[2],
-                                  macAddress[3], macAddress[4], macAddress[5]];
-
-    // Release the buffer memory
-    free(msgBuffer);
-
-    return macAddressString;
+    // Keep the format of the device IDs generated before: uppercase hex digits without separators
+    return [[macAddress stringByReplacingOccurrencesOfString:@":" withString:@""] uppercaseString];
 }
 #endif
 
